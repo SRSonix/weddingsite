dd5322d8ab9e3f3bd06b47329ab116256c518d0a
Michael Meißner <meissner@twaice.com>
2025-06-25 00:11:49 +0200

    add deployment code for app; add some readme

---
+++ README.md
@@ -0,0 +1,40 @@
+# Running locally
+
+# run app locally
+* install node dependencies for app: (in app folder)
+```sh
+npm install 
+```
+* Fill in VITE_API_URL with desired backend in app/.env (example env file provided).
+* run app locally (in app folder)
+```sh
+npm run dev
+```
+* the app will be available on [localhost:5173](http://localhost:5173/)
+
+# run app & backend
+* Fill in VITE_API_URL with localhost:5000 in app/.env (example env file provided).
+* run docker with local containers for db, db-admin, app & api:
+```sh
+docker-compose -f compose.yaml up --build 
+```
+
+# Deployment 
+
+## deploy app
+* install node dependencies for deployment: (in deployment folder)
+```sh
+npm install 
+```
+* Fill in FTP_HOST, WEBSITE_ADMIN_USE and WEBSITE_ADMIN_PW in deployment/.env (example env file provided).
+* build app: (in app folder)
+```sh
+npm run build
+```
+* deploy previously build app: (in deployment folder)
+```sh
+npm run deploy-app
+```
+
+## deploy api
+Currently only manual deployment available.
---
+++ deployment/deploy_app.ts
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv';
+import { Client } from "basic-ftp"
+
+dotenv.config();
+
+const FTP_HOST = process.env.FTP_HOST
+const WEBSITE_ADMIN_USER = process.env.WEBSITE_ADMIN_USER
+const WEBSITE_ADMIN_PW = process.env.WEBSITE_ADMIN_PW
+
+const SRC_BASE_PATH = "../app/build/client/"
+
+async function upload_weddingsite() {
+    const client = new Client()
+    try {
+        await client.access({
+            host: FTP_HOST,
+            user: WEBSITE_ADMIN_USER,
+            password: WEBSITE_ADMIN_PW,
+            secure: false // set to true if using FTPS
+        })
+        for (const file_name of ["index.html", ".htaccess"]){
+            console.log("Uploading file " + file_name)
+
+            await client.remove(file_name, true)
+            await client.uploadFrom(SRC_BASE_PATH + file_name, "/"+ file_name)
+        }
+
+        for (const directory_name of ["assets", "translations"]){
+            console.log("Uploading directory " + directory_name)
+
+            await client.removeDir(directory_name)
+            await client.uploadFromDir(SRC_BASE_PATH + directory_name, "/"+ directory_name)
+        }
+
+        console.log("Upload successful")
+    } catch (err) {
+        console.error("FTP upload failed:", err)
+    }
+    client.close()
+}
+
+upload_weddingsite()
