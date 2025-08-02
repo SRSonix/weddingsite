41e9db01f50d09e1c34bb2401b218cec1df74b69
Michael Meißner <meissner@twaice.com>
2025-08-03 01:05:19 +0200

    add initial version of itiniaries

---
+++ app/app/components/traveling/itineraries.tsx
@@ -0,0 +1,87 @@
+
+export default function Itineraries(){
+return (
+    <div className="text-justify">
+        <h4>Yucatan</h4>
+        <div className="text-justify">
+            Although this is a small part of Mexico, we recommend all those who have not explored it yet to focus on this peninsula. Consisting of the two states of Yucatan and Quintana Roo, it is not only one of the safest regions of the country, but also has a whole lot to offer in terms of nature, culture, history, food and entertainment. It is also easy to travel with the brand new Tren Maya, as well as the many bus routes that run across the area.
+        </div>
+        <div className="text-justify">
+            Unless you are planning to stay for more than two weeks, we recommend staying in this region, plus Mexico City if that is where you are flying from/to.
+        </div>
+
+        <h4>Tren Maya</h4>
+        <div className="text-justify">
+            Especially if you have not travelled Yucatan yet, Tren Maya makes exploring the peninsula easier than ever. Constructed in about 3 years (or 1/10 of the time it took to build Berlin’s new airport), it did cause significant environmental damage. The new government is making some efforts to repair the damage done, but in any case, it is now the most sustainable and comfortable way to get around Yucatan. (If budget is a priority, buses provide a cheaper option, but the train is quite accessible.)
+        </div>
+        <div className="text-justify">
+            The train forms a loop around the peninsula, and since the itineraries are constructed around that loop, you can adjust the direction of travel and your starting point to suit your arrival and departure dates, relative to the date of the wedding.
+        </div>
+
+        <h4>10-14 day itinerary</h4>
+        <h4>Day 1: Cancun &amp; Arrival</h4>
+        <div className="text-justify">
+            Unless you want to party with Americans, you can travel straight on and not look back. If you land late, stay the night so it’s less stressful; otherwise, take the first train on to Playa del Carmen (50 mins), and then hop on the ferry to Cozumel.
+        </div>
+
+        <h4>Days 1-4: Cozumel</h4>
+        <div className="text-justify">
+            This is a great place to ease into your journey and shed the jetlag. Explore San Miguel, the lively port town, with a Michelada in hand, and enjoy the spectacular nature. Relaxing at one of the island’s beautiful beaches, or go for a snorkel or dive to experience pristine marine life. If you’ve been thinking of getting your diver’s license, this is a great place to do it - for a better price and in a more beautiful environment than you will find in Europe.
+        </div>
+        <div className="text-justify">
+            Highlights of Cozumel include the El Cielo Sandbar, Colombia Deep Reef, Santa Rosa Reef, and the San Gervasio archeological site.
+        </div>
+        <div className="text-justify">
+            3 nights should be enough, though an extra night will make it easier if you want to get your diver’s license. If you want to shorten your itinerary, you can also cut a day of your stay here.
+        </div>
+
+        <h4>Day 4: Tulum</h4>
+        <div className="text-justify">
+            Though a tourist hotspot, the small town of Tulum has something unique: ruins by the sea. This can be a short stop, but at just 1 hour by train from Playa del Carmen, it is worth stopping by this Mayan site that is conveniently on your route. Perched atop 12 meter cliffs overlooking the caribbean sea, you can walk through the well-preserved buildings, inhabited today by iguanas. At the end of the tour, you will land on a beautiful beach, where you can sunbathe and enjoy a refreshing dip in the sea. Be sure to bring sunscreen, and try to time your visit so you don’t end up on the beach in the full noon sun.
+        </div>
+        <div className="text-justify">
+            If you take the morning train, you will have about 5 hours to explore the ruins and enjoy the beach, then continue with the 3 hour evening train (or bus) to Bacalar.
+        </div>
+
+        <h4>Days 4-6: Bacalar</h4>
+        <div className="text-justify">
+            Bacalar is a small town that attracts especially those fond of nature. The highlight for most is surely the lagoon: an expansive body of crystal clear water (depending on weather), with beautiful shades of colour that is perfect for visiting on a kayak. You can also go on a sailboat, if you want to cover more ground. This is one of the most pleasant places we have ever swam in. Some hotels also have private access to the lagoon, which is surely beautiful but will drive up the price.
+        </div>
+        <div className="text-justify">
+            Beyond the lagoon, there are various natural sites and ruins, such as Kohunlich and Chacchoban, that you can visit. But for this, you will need more time here. One great option is the <a href="https://www.google.com/maps/search/cenote/@18.6361746,-88.4913967,12.5z?hl=en-AT&entry=ttu&g_ep=EgoyMDI1MDcyMy4wIKXMDSoASAFQAw%3D%3D" target="_blank">Hotel Kohunlich Explorean</a>, which includes tours to Bacalar lagoon, Kohunlich ruins and other locations in your stay.
+        </div>
+
+        <h4>Days 6-8: Xpujil and Calakmul</h4>
+        <div className="text-justify">
+            Xpujil is less than 2 hours by train from Bacalar. The village of Xpujil is not in itself a significant site to visit. It is, however, the gateway to the Calakmul nature reserve and ruins.
+        </div>
+        <div className="text-justify">
+            As it is not close to any other major town or site, this is often skipped by visitors. But it is very much worth the trip, and staying a night in Xpujil will allow you to have a full day experience of the reserve, and immerse yourself into the beautiful scenery. The area is teeming with wildlife, including monkeys, tapirs, over 400 bird species, and the elusive Jaguar, as well as amazing plants such as orchids, mahogany and more.
+        </div>
+        <div className="text-justify">
+            Equally spectacular are the Calakmul ruins. Once one of the most powerful cities in the regions, it was absorbed by the jungle until rediscovery in the early 1930s. Today, you explore a lot of the vast site, and even climb the pyramid for a rewarding view of the jungle canopy. Trees grow over the ruins, giving them a very mysterious and adventurous feel. And thanks to it being hard to reach, it is not as busy as most of the other sites. If you enjoy nature and are interested in ruins, this is a highly recommendable stop on your journey.
+        </div>
+
+        <h4>Days 8-11: Palenque</h4>
+        <div className="text-justify">
+            5 hours by train from Xpujil, another spectacular ruins-cum-nature reserve awaits. It is likely a lot busier than Xpujil, but worth it. First, there are the Palenque ruins. Dating back to around 600 AD, they are among the most impressive ruins you will see in Mexico.
+        </div>
+        <div className="text-justify">
+            Nature in Palenque is also pristine: the Roberto Barrios waterfalls and Aguas Azules create gorgeous natural pools which you can bathe in, and the Misol-Ha Waterfall drops from an impressive 35 metres, with a sort of cave behind it you can walk along.
+        </div>
+
+        <h4>Days 11-14: Mérida &amp; Wedding</h4>
+        <div className="text-justify">
+            The 7 hour train ride to Mérida is the longest leg on this itinerary. We recommend staying at least 3 nights in Mérida as it is a great city to experience Yucatecan life, with an amazing offering of local food, characteristic architecture and the sea nearby. With the wedding taking about half a day, and an optional beach day together the next day, you will want some time to dedicate to this lovely city.
+        </div>
+        <div className="text-justify">
+            As the wedding will be a 20-60 minute drive from the city depending on traffic, we recommend finding a hotel in the city centre, so you can immerse yourself in the vibrant flair of Mérida. Take a walking tour, and explore the local scene with its markets, restaurants and bars.
+        </div>
+
+        <h4>Day 14: Cancun &amp; Departure</h4>
+        <div className="text-justify">
+            Again, not much to do in Cancun. Fortunately, there is a direct train to the airport, so you don’t have to bother with the city. The earliest one arrives there at 11:30 AM, so unless that is too late for your flight back, we recommend enjoying your last night in Mérida, before heading home again.
+        </div>
+    </div>
+    )
+}
--- app/app/routes/traveling.tsx
+++ app/app/routes/traveling.tsx
@@ -2,6 +2,8 @@ import type { Route } from "./+types/traveling";
 import { useTranslation } from "react-i18next";
 import { EmptyState } from "~/components/common/empty_state";
 import { ComingSoon } from "~/components/common/coming_soon";
+import Itineraries from "~/components/traveling/itineraries";
+import { ContentTile } from "~/components/common/content_tile";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,14 +17,22 @@ export default function Traveling() {
 
   return (
     <EmptyState>
-      <ComingSoon>
-        <ul className="list-disc list-inside">
-          <li>{t("recommended-sites")}</li>
-          <li>{t("itineraries")}</li>
-          <li>{t("food")}</li>
-          <li>{t("culture")}</li>
-        </ul>
-      </ComingSoon>
+
+      <div className="content-tile-wrap">
+      <ContentTile header={undefined}>
+        <ComingSoon>
+          <ul className="list-disc list-inside">
+            <li>{t("recommended-sites")}</li>
+            <li>{t("food")}</li>
+            <li>{t("culture")}</li>
+          </ul>
+        </ComingSoon>
+      </ContentTile>
+
+      <ContentTile header="Itineraries" >
+        <Itineraries/>
+      </ContentTile>
+      </div>
     </EmptyState>
   )
 }
